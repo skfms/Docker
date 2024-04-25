05f59fab1117f838a9af971b4e677fa0d141e8e6
skfms <leegs11@gmail.com>
2024-04-26 08:35:21 +0900

    WSL 에서 Docker 가 종료 후 Port가 잠겨있는 문제 처리

--- MariaDB/run_MariaDB.sh
+++ MariaDB/run_MariaDB.sh
@@ -1,8 +1,31 @@
-podman run -d --name mariadb \
-  -p 127.0.0.1:3306:3306 \
-  -v ~/Docker/MariaDB/data:/var/lib/mysql \
-  -v ~/Docker/MariaDB/log:/var/log/mysql \
-  -e TZ=Asia/Seoul \
-  -e MARIADB_ROOT_PASSWORD=pwd \
-  -e MARIADB_DATABASE=test \
-  docker.io/library/mariadb
+#!/bin/sh
+
+cname="mariadb";
+volnm="-v /home/leegs//Docker/MariaDB/data:/var/lib/mysql -v /home/leegs/Docker/MariaDB/log:/var/log/mysql";
+env="-e TZ=Asia/Seoul -e MARIADB_ROOT_PASSWORD=pwd -e MARIADB_DATABASE=test";
+
+case "$1" in
+        'start')
+                podman run -d -p 127.0.0.1:3306:3306 --name $cname $env --hostname $cname $volnm $cname
+                ;;
+
+        'stop')
+                podman stop $cname && podman rm $cname
+				pnum=`netstat -nltp 2> /dev/null | grep 3306 | awk {'print $7;}' | awk -F'/' '{print $1;}'`
+				kill $pnum
+                ;;
+
+        'status')
+                ;;
+
+        *)
+                echo "Usaage: $0 {start | stop | status}";
+                exit 1;
+                ;;
+
+esac
+
+echo ''
+podman ps
+
+exit 0;
