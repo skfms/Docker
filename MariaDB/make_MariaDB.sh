026c0cf8128ecaa1288fbc99cc21caa58417871d
skfms <leegs11@gmail.com>
2024-05-17 17:59:30 +0900

    mariadb 스크립트 수정

---
+++ MariaDB/make_MariaDB.sh
@@ -0,0 +1,1 @@
+podman build -t mariadb -f MariaDB.dockerfile .
--- MariaDB/run_MariaDB.sh
+++ MariaDB/run_MariaDB.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 cname="mariadb";
-volnm="-v /home/leegs//Docker/MariaDB/data:/var/lib/mysql -v /home/leegs/Docker/MariaDB/log:/var/log/mysql";
+volnm="-v /home/leegs/Docker/MariaDB/data:/var/lib/mysql";
 env="-e TZ=Asia/Seoul -e MARIADB_ROOT_PASSWORD=pwd -e MARIADB_DATABASE=test";
 
 case "$1" in
         'start')
+				echo "***Start $cname !"
                 podman run -d -p 127.0.0.1:3306:3306 --name $cname $env --hostname $cname $volnm $cname
+				podman ps -f name=$cname
                 ;;
 
         'stop')
-                podman stop $cname && podman rm $cname
-				pnum=`netstat -nltp 2> /dev/null | grep 3306 | awk {'print $7;}' | awk -F'/' '{print $1;}'`
-				kill $pnum
+                podman stop $cname > /dev/null && podman rm $cname > /dev/null
+				echo "***Stop $cname !"
+				#pnum=`netstat -nltp 2> /dev/null | grep 3306 | awk {'print $7;}' | awk -F'/' '{print $1;}'`
+				#kill $pnum
                 ;;
 
         'status')
+				podman ps -f name=$cname
                 ;;
 
         *)
@@ -24,8 +28,5 @@ case "$1" in
                 ;;
 
 esac
-
-echo ''
-podman ps
 
 exit 0;
