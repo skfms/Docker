76805e8ec06ea1a510e198b2aff7c37eeda8522f
skfms <leegs11@gmail.com>
2024-11-08 10:47:38 +0900

    Oracle 12 실행 모듈 수정

--- Oracle/12/run_ora12.sh
+++ Oracle/12/run_ora12.sh
@@ -6,16 +6,16 @@ volnm="-v ./data/oradata:/app/oracle/oradata";
 case "$1" in
 	'start')
 		podman run -itd -p 1512:1521 --name $cname --hostname $cname $volnm $cname
+		echo "*** Start $cname !"
 		;;
 
 	'stop')
-		podman stop $cname && podman rm $cname
-		exit 0;
-		#pnum=`netstat -nltp 2> /dev/null | grep 1521 | awk {'print $7;}' | awk -F'/' '{print $1;}'`
-		#kill $pnum
+		podman rm -f $cname
+		echo "*** Stop $cname !"
 		;;
 
 	'status')
+		podman ps -f name=$cname
 		;;
 
 	*)
@@ -24,8 +24,5 @@ case "$1" in
 		;;
 
 esac
-
-echo ''
-podman ps
 
 exit 0;
