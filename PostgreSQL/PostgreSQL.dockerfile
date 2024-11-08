9705f3a6240dfc2c3844ab92e60daa1492374be1
skfms <leegs11@gmail.com>
2024-11-08 10:50:16 +0900

    PotgreSQL 스크립트 수정

--- PostgreSQL/PostgreSQL.dockerfile
+++ PostgreSQL/PostgreSQL.dockerfile
@@ -1,4 +1,4 @@
-FROM docker.io/library/postgres
+FROM docker.io/library/postgres:16.4
 
 #TimeZone Setting
 RUN ln -snf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
--- PostgreSQL/run_PostgreSQL.sh
+++ PostgreSQL/run_PostgreSQL.sh
@@ -1,32 +1,29 @@
 #!/bin/sh
 
 cname="postgres";
-volnm="-v /home/leegs/Docker/PostgreSQL/data:/var/lib/postgres/data";
+volnm="-v /home/leegs/Docker/PostgreSQL/data/pgdata:/var/lib/postgresql/data postgres";
 env="-e TZ=Asia/Seoul -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRESQL_PASSWORD=pwd -e PGDATA=/var/lib/postgresql/data/pgdata";
 
 case "$1" in
-        'start')
-				echo "*** Start $cname !"
-                podman run -d -p 127.0.0.1:5432:5432 --name $cname $env --hostname $cname $volnm $cname
-				podman ps -f name=$cname
-                ;;
+	'start')
+		echo "*** Start $cname !"
+		podman run -d -p 127.0.0.1:5432:5432 --name $cname $env --hostname $cname $volnm $cname
+		podman ps -f name=$cname
+		;;
 
-        'stop')
-                podman stop $cname > /dev/null  && podman rm $cname > /dev/null
-				echo "*** Stop $cname !"
-				#pnum=`netstat -nltp 2> /dev/null | grep 3306 | awk {'print $7;}' | awk -F'/' '{print $1;}'`
-				#kill $pnum
-                ;;
+	'stop')
+		podman stop $cname > /dev/null  && podman rm $cname > /dev/null
+		echo "*** Stop $cname !"
+		;;
 
-        'status')
-				podman ps -f name=$cname
-                ;;
+	'status')
+		podman ps -f name=$cname
+		;;
 
-        *)
-                echo "Usaage: $0 {start | stop | status}";
-                exit 1;
-                ;;
-
+	*)
+		echo "Usaage: $0 {start | stop | status}";
+		exit 1;
+		;;
 esac
 
 exit 0;
